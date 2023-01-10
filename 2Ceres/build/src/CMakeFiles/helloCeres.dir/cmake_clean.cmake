cdb57e3b6b4a85522e7e20c9bcfa5f60206c125b
struggler <wylmkthhistory@163.com>
2023-01-10 19:25:51 +0800

    ceres

---
+++ 2Ceres/build/src/CMakeFiles/helloCeres.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloCeres.dir/1HelloCeres.cc.o"
+  "helloCeres"
+  "helloCeres.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloCeres.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
